059e82edf66a0783577869c78172c29ee3c53883
ZzxDDDDDD <120468164+ZzxDDDDDD@users.noreply.github.com>
2024-04-23 23:24:55 +0800

    Add html folder.

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -119,7 +119,7 @@ const Register = () => {
                 <section>
                     <h1>Success!</h1>
                     <p>
-                        <a href="/">Sign In</a>
+                        <a href="/admin">Sign In</a>
                     </p>
                 </section>
             ) : (
